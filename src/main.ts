26c3ca6c8d437c6f8a635324b62d9073af36912a
remorses <beats.by.morse@gmail.com>
2019-09-19 23:16:21 +0200

    added node mods

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import getChangedDirs from './getChangedDirs'
 
 async function run() {
     console.log(JSON.stringify(process.env, null, '    '))
+    if (!process.env.GITHUB_SHA) {
+        core.setFailed('cannot access sha')
+    }
     const dirs = await getChangedDirs('.', process.env.GITHUB_SHA)
     core.setOutput('changedDirs', JSON.stringify(dirs)) // steps.<step id>.outputs
+    core.exportVariable('changedDirs', dirs.join(', '))
 }
 
 
